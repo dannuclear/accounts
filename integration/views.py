73b41f1a4e13dd5458ab8e209fd1c4eb0c356f2c
Denis Vaganov <vaganov@bi-soft.ru>
2025-06-10 14:21:56 +0500

    fix load

--- integration/views.py
+++ integration/views.py
@@ -174,7 +174,8 @@ def createPrepaymentFromOrder(request, id):
     prep.status_id = 1
     prep.save()
 
-    intPrep = Prepayment.objects.filter(xv26eiId=order.estimateId, empOrgNo=empOrgNo).first()
+    #intPrep = Prepayment.objects.filter(xv26eiId=order.estimateId, empOrgNo=empOrgNo).first()
+    intPrep = Prepayment.objects.filter(orderId=order.id).first()
     if intPrep is not None:
         obtain_method = ObtainMethod.objects.filter(bik=intPrep.bic).first()
         prepItem = prepaymentModels.PrepaymentItem()
